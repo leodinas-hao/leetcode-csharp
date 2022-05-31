05c87c90f3030d1eaacad007ad9c3c7dba0b3ac1
Leo Hao <lhao@macquarietelecom.com>
2022-05-31 16:42:57 +1000

    feat: add split array largest sum

---
+++ leetcode/SplitArrayLargestSum.cs
@@ -0,0 +1,99 @@
+/*
+Split Array Largest Sum
+
+Given an array nums which consists of non-negative integers and an integer m, you can split the array into m non-empty continuous subarrays.
+
+Write an algorithm to **minimize** the largest sum among these m subarrays.
+
+Example 1:
+Input: nums = [7,2,5,10,8], m = 2
+Output: 18
+Explanation:
+There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8],
+where the largest sum among the two subarrays is only 18.
+
+Example 2:
+Input: nums = [1,2,3,4,5], m = 2
+Output: 9
+
+Example 3:
+Input: nums = [1,4,4], m = 3
+Output: 4
+
+Constraints:
+1 <= nums.length <= 1000
+0 <= nums[i] <= 10^6
+1 <= m <= min(50, nums.length)
+*/
+
+namespace LeetCode.SplitArrayLargestSum;
+
+
+public class Solution
+{
+  public int SplitArray(int[] nums, int m)
+  {
+    // find the boundary of the result
+    // between max element & total sum
+    int maxElement = -1;
+    int totalSum = -1;
+    for (int i = 0; i < nums.Length; i++)
+    {
+      maxElement = Math.Max(nums[i], maxElement);
+      totalSum += nums[i];
+    }
+
+    // try to find a solution, which can meet the best guessing boundary
+    int left = maxElement;
+    int right = totalSum;
+    int minimumLargestSum = -1;
+    while (left <= right)
+    {
+      // get a mid value, which can be the best guessing minimized max sum
+      int maxSumAllow = left + (right - left) / 2;
+
+      // find the minimum splits
+      // if split is less than or equal to "m", move the the maxSumAllow to smaller
+      // otherwise increase maxSumAllow
+      if (MinimumSplits(nums, maxSumAllow) <= m)
+      {
+        // there might be a better solution/split
+        // try to decrease the boundary
+        right = maxSumAllow - 1;
+        minimumLargestSum = maxSumAllow;
+      }
+      else
+      {
+        // need to increase the guessing to make the required split
+        left = maxSumAllow + 1;
+      }
+    }
+
+    return minimumLargestSum;
+  }
+
+  private int MinimumSplits(int[] nums, int maxSumAllow)
+  {
+    int sum = 0;
+    int splits = 0;
+
+    foreach (int n in nums)
+    {
+      if (sum + n <= maxSumAllow)
+      {
+        // not exceeding the max sum, keep adding to this split
+        sum += n;
+      }
+      else
+      {
+        // exceeded the max sum, more split required
+        sum = n;  // reset the sum
+        splits++;
+      }
+    }
+
+    // return number of subarrays, which is splits + 1
+    return splits + 1;
+  }
+}
---
+++ tests/SplitArrayLargestSumTests.cs
@@ -0,0 +1,16 @@
+using LeetCode.SplitArrayLargestSum;
+
+namespace tests;
+
+public class SplitArrayLargestSumTests
+{
+  [Theory]
+  [InlineData(new int[] { 7, 2, 5, 10, 8 }, 2, 18)]
+  [InlineData(new int[] { 1, 2, 3, 4, 5 }, 2, 9)]
+  [InlineData(new int[] { 1, 4, 4 }, 3, 4)]
+  public void Test1(int[] nums, int m, int except)
+  {
+    int result = new Solution().SplitArray(nums, m);
+    Assert.Equal(except, result);
+  }
+}
