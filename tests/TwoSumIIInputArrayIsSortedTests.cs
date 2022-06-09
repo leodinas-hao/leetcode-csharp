df99ff57ed9c03d36e2922b67d49277b7d0d3885
Leo Hao <lhao@macquarietelecom.com>
2022-06-09 15:12:14 +1000

    feat: add two sum II

---
+++ tests/TwoSumIIInputArrayIsSortedTests.cs
@@ -0,0 +1,18 @@
+using LeetCode.TwoSumIIInputArrayIsSorted;
+
+namespace tests;
+
+public class TwoSumIIInputArrayIsSorted
+{
+  [Theory]
+  [InlineData(new int[] { 2, 7, 11, 15 }, 9)]
+  [InlineData(new int[] { 2, 3, 4 }, 6)]
+  [InlineData(new int[] { -1, 0 }, -1)]
+  [InlineData(new int[] { 5, 25, 75 }, 100)]
+  public void Test1(int[] numbers, int target)
+  {
+    var result = new Solution().TwoSum(numbers, target);
+    Assert.Equal(2, result.Length);
+    Assert.Equal(target, numbers[result[0] - 1] + numbers[result[1] - 1]);
+  }
+}
