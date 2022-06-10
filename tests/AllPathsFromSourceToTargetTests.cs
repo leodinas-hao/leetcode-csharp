926389f8ebacdf83146b4e2ca8068c5eb92114c0
Leo Hao <lhao@macquarietelecom.com>
2022-06-10 13:05:10 +1000

    feat: add all paths from source to target

---
+++ tests/AllPathsFromSourceToTargetTests.cs
@@ -0,0 +1,50 @@
+using LeetCode.AllPathsFromSourceToTarget;
+
+namespace tests;
+
+public class AllPathsFromSourceToTargetTests
+{
+  public static IEnumerable<object[]> GetTestData()
+  {
+    yield return new object[]{
+      new int[][] {
+        new int[]{1,2},
+        new int[]{3},
+        new int[]{3},
+        new int[]{},
+      },
+      new int[][] {
+        new int[]{0,1,3},
+        new int[]{0,2,3},
+      }
+    };
+    yield return new object[]{
+      new int[][] {
+        new int[]{4,3,1},
+        new int[]{3,2,4},
+        new int[]{3},
+        new int[]{4},
+        new int[]{},
+      },
+      new int[][] {
+        new int[]{0,4},
+        new int[]{0,3,4},
+        new int[]{0,1,3,4},
+        new int[]{0,1,2,3,4},
+        new int[]{0,1,4},
+      }
+    };
+  }
+
+  [Theory]
+  [MemberData(nameof(GetTestData))]
+  public void Test1(int[][] graph, int[][] expect)
+  {
+    var result = new Solution().AllPathsSourceTarget(graph);
+    Assert.Equal(result.Count, expect.Length);
+    foreach (var row in expect)
+    {
+      Assert.True(result.Any(r => Enumerable.SequenceEqual(r, row)));
+    }
+  }
+}
